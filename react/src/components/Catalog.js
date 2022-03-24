b63cd2a498392f5725988c774bd7a6ae48dc34c3
Roberto Díaz <robertocdiazsanchez@gmail.com>
2022-03-24 16:51:35 -0500

    main screen ui seems finished

--- react/src/components/Catalog.js
+++ react/src/components/Catalog.js
@@ -1,17 +1,18 @@
 import Movie from "./Movie";
+import faker from '@faker-js/faker';
 
 const Catalog = () => {
+    const movies = [];
+    for (let i = 0; i < Math.floor(Math.random() * 5 + 7); ++i) {
+        movies.push({
+            title: faker.company.companyName(),
+            watched: Math.random() < 0.5
+        });
+    }
+
     return (
         <div className="Catalog">
-            <Movie />
-            <Movie />
-            <Movie />
-            <Movie />
-            <Movie />
-            <Movie />
-            <Movie />
-            <Movie />
-            <Movie />
+            {movies.map(m => <Movie title={m.title} watched={m.watched} />)}
         </div>
     );
 }
--- react/src/components/Movie.js
+++ react/src/components/Movie.js
@@ -1,10 +1,16 @@
-const Movie = () => {
+const Movie = ({ title, watched }) => {
+    const handleClick = () => {
+        
+    };
+
     return (
-        <div className="Movie">
+        <div className={`Movie ${watched ? 'watched' : ''}`} onClick={handleClick}>
             <img src="https://m.media-amazon.com/images/M/MV5BYzg0NGM2NjAtNmIxOC00MDJmLTg5ZmYtYzM0MTE4NWE2NzlhXkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_FMjpg_UX1000_.jpg" alt="" />
+            <div className="gradient" />
             <p className="title">
-                Movie title
+                {title ?? 'What if the title of the movie gets too long?'}
             </p>
+            {watched && <span className="seen-icon material-icons">done</span>}
         </div>
     );
 }
